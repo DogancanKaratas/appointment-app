bcc15ac7c09f67816ba026b0164644555b5a06db
DogancanKaratas <karatasdgncn21@gmail.com>
2023-12-11 23:01:07 +0300

    Refactor Home component to use localStorage for storing appointments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,24 @@
-import AppointmentList from "../components/AppointmentList"
-import Doctors from "../components/Doctors"
-import { appointmentData } from "../helper/data"
-import { useState } from "react"
+import AppointmentList from "../components/AppointmentList";
+import Doctors from "../components/Doctors";
+import { appointmentData } from "../helper/data";
+import { useState } from "react";
 
 const Home = () => {
-  const [appointments, setAppointments] = useState(appointmentData);
+  const [appointments, setAppointments] = useState(
+    JSON.parse(localStorage.getItem("list")) || []
+  );
   const handleAdd = (newAppointment) => {
     setAppointments([...appointments, newAppointment]);
+    localStorage.setItem(
+      "list",
+      JSON.stringify([...appointments, newAppointment])
+    );
   };
 
   const handleDelete = (id) => {
     const filteredList = appointments.filter((item) => item.id !== id);
     setAppointments(filteredList);
+    localStorage.setItem("list", JSON.stringify([filteredList]));
   };
 
   const handleDoubleClick = (id) => {
@@ -19,6 +26,7 @@ const Home = () => {
       item.id === id ? { ...item, consulted: !item.consulted } : item
     );
     setAppointments(updatedList);
+    localStorage.setItem("list", JSON.stringify([updatedList]));
   };
   return (
     <main className="text-center mt-2">
@@ -33,4 +41,4 @@ const Home = () => {
     </main>
   );
 };
-export default Home
+export default Home;
